1771d2a524a2a91e50eec65beee6556914860b19
fausto guillen and manasa vesala <fausto@>
2018-10-16 13:05:58 -0700

    added some css

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(".js").click(function(){
+        $(".bg1").toggle();
+    });
+
+    $(".vr").click(function(){
+        $(".bg2").toggle();
+    });
+
+    $(".vrm").click(function(){
+        $(".bg3").toggle();
+    });
+
+    $(".fun").click(function(){
+        $(".bg4").toggle();
+    });
+
+    $(".str").click(function(){
+        $(".bg5").toggle();
+    });
+
+    $(".alt").click(function(){
+        $(".bg6").toggle();
+    });
+
+});
